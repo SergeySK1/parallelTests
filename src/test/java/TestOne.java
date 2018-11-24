a074a49617514eb25909af8684f57e4f2f929c68
Ruslan Sheyko <ruslan.sheyko@ihsmarkit.com>
2018-11-24 16:47:37 +0300

    create tests parallel

---
+++ src/test/java/TestOne.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
+public class TestOne extends SetupDriver {
+
+    private String browserName="";
+    private WebDriver driver;
+
+    @BeforeTest()
+    @Parameters({"browserName"})
+    public void initBeforeTest(String browserName){
+        this.browserName = browserName;
+        driver = getDriver(browserName);
+    }
+
+    @BeforeClass()
+    public void setup(){
+        driver.get("https://www.yandex.ru/");
+    }
+
+
+    @Test(priority = 1)
+    public void oneTest(){
+        driver.findElement(By.id("text")).sendKeys("Погода");
+        driver.findElement(By.cssSelector(".search2__button")).click();
+        driver.findElement(By.cssSelector(".logo_type_link")).click();
+    }
+
+    @Test(priority = 2)
+    public void twoTest(){
+        driver.findElement(By.id("text")).sendKeys("Автомобили");
+        driver.findElement(By.cssSelector(".search2__button")).click();
+        driver.findElement(By.cssSelector(".logo_type_link")).click();
+    }
+
+    @AfterClass()
+    public void removeDriver(){
+        super.removeDriver();
+    }
+
+}
